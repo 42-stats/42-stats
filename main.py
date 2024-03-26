133114ecc56183775978e93c6f88075215a8b48f
Simon Dablander <sdabland@student.42vienna.com>
2024-03-26 21:33:36 +0100

    style: add black formatter
    Signed-off-by: Simon Dablander <sdabland@student.42vienna.com>

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from src.CLInterface    import Interface
+from src.CLInterface import Interface
+
 
 def main():
     try:
         Interface()
     except Exception as e:
-        print('\r\nunhandled error:', e, 'please open an issue at https://github.com/winstonallo/42-stats/issues')
+        print(
+            "\r\nunhandled error:",
+            e,
+            "please open an issue at https://github.com/winstonallo/42-stats/issues",
+        )
         return 1
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-        
--- src/animation_utils.py
+++ src/animation_utils.py
@@ -1,14 +1,16 @@
 import threading
 import time
 
+
 def start_animation(done_event: threading.Event, message: str):
     def animation():
-        chars = ['|', '/', '-', '\\']
+        chars = ["|", "/", "-", "\\"]
         idx = 0
         while not done_event.is_set():
-            print(f'\r{message} {chars[idx % len(chars)]}', end="")
+            print(f"\r{message} {chars[idx % len(chars)]}", end="")
             idx += 1
             time.sleep(0.1)
+
     loading_thread = threading.Thread(target=animation)
     loading_thread.start()
     return loading_thread
