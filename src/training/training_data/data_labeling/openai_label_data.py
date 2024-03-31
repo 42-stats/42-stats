2703f688ea646109b80df511aab69caac7c71ea2
Arthur Bied-Charreton <arthurbiedchar@gmail.com>
2024-03-31 14:14:53 +0200

    doubt: idk if this is working tbh

--- src/training/training_data/data_labeling/openai_label_data.py
+++ src/training/training_data/data_labeling/openai_label_data.py
@@ -20,15 +20,29 @@ def generate_text_labels_batch(texts):
             messages=[
                 {
                     "role": "system",
-                    "content": "You are evaluating feedback from a coding school. Constructive feedback typically identifies specific strengths and areas for improvement, offers concrete suggestions, and encourages positive development. Feedback that lacks detail, does not relate to specific actions, or fails to offer guidance for improvement is less constructive. One exception is when the student pushed an empty repository/empty folder - Please rate this with -1 so I can filter them out.Your task is to rate feedback based on its constructiveness.",
+                    "content": """You are evaluating feedback from a coding school.
+                    Constructive feedback typically identifies specific strengths and
+                    areas for improvement, offers concrete suggestions, and encourages
+                    positive development. Feedback that lacks detail, does not relate
+                    to specific actions, or fails to offer guidance for improvement is less
+                    constructive.
+                    One exception is when the student pushed an empty repository/empty folder
+                    - Please rate this with -1 so I can filter them out.
+                    Your task is to rate feedback based on its constructiveness.""",
                 },
                 {
                     "role": "user",
-                    "content": f"{text}; Considering the criteria for constructive feedback, rate this 42 Coding School evaluation feedback on a scale from 0 (completely out of place and not helpful) to 9 (very constructive, offering specific, actionable, and encouraging guidance). Only provide a number from 0 to 9. Be strict in your evaluation; the goal here is to identify feedback that genuinely contributes to improvement. Remember, a '9' should be reserved for feedback that is exemplary in specificity, helpfulness, and encouragement, while a '0' indicates feedback that is not applicable or useful for development.",
+                    "content": f"""{text}; Considering the criteria for constructive feedback,
+                    rate this 42 Coding School evaluation feedback on a scale from 0
+                    (completely out of place and not helpful) to 9 (very constructive, offering
+                    specific, actionable, and encouraging guidance). Only provide a number from
+                    0 to 9. Be strict in your evaluation; the goal here is to identify feedback
+                    that genuinely contributes to improvement. Remember, a '9' should be reserved
+                    for feedback that is exemplary in specificity, helpfulness, and encouragement,
+                    while a '0' indicates feedback that is not applicable or useful for development.""",
                 },
             ],
         )
-
 
         label = response.choices[0].message.content
         labels.append(label)
@@ -43,11 +57,11 @@ def generate_labels_for_dataframe(df, text_column_name):
 
     for index, row in df.iterrows():
         batch_texts.append(row[text_column_name])
-
         if len(batch_texts) == BATCH_SIZE:
             labels, _ = generate_text_labels_batch(batch_texts)
             new_data.extend(zip(batch_texts, labels))
             batch_texts = []
+        print(f"\rLabels generated: {index + 1}", end="", flush=True)
 
     if batch_texts:
         labels, _ = generate_text_labels_batch(batch_texts)
@@ -57,6 +71,7 @@ def generate_labels_for_dataframe(df, text_column_name):
     labels_df.to_csv("labeled_data.csv", index=False)
 
 
-# df = pd.read_csv("src/training/training_data/data_labeling/vienna_all_eval_comments.csv")
-df = pd.read_csv("src/training/training_data/data_labeling/test.csv")
+df = pd.read_csv(
+    "src/training/training_data/data_labeling/vienna_all_eval_comments.csv"
+)
 generate_labels_for_dataframe(df, "comment")
