a0f4e8a9bf8d98690ced7892b477bc36d2c5d8fa
Nicol Weghofer <nweghofe@c3r7p1.42vienna.com>
2024-08-18 14:34:25 +0200

    fix: use dynamic file name based on evaluation side

--- src/utils.py
+++ src/utils.py
@@ -161,7 +161,7 @@ class Utils:
             evaluations.extend(data)
             page += 1
 
-        with open("evals.json", "w") as f:
+        with open(f"{side}.json", "w") as f:
             json.dump(evaluations, f, indent=4)
         df = pd.DataFrame(evaluations)
         return df
